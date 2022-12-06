a0f71ab53b879cef5c5e4bcb51de97ca41190adf
-l <alexmaimartfilho@gmail.com>
2022-12-06 18:39:08 -0300

    Inicio MPI

---
+++ MPI/mpi.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<mpi.h>
+
+int main(int argc, char ** argv){
+
+int numtask, rank, rc;
+
+rc = MPI_Init(&argc,&argv);
+//Teste Segurança
+if(rc != MPI_SUCCESS){
+    printf("Error start MPI Program. Terminating..\n");
+    MPI_Abort(MPI_COMM_WORLD,rc);
+}
+MPI_Comm_size(MPI_COMM_WORLD,&numtask);
+MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+printf("Numero de task:%d Meu Ranking:%d\n",numtask,rank);
+    printf("Hello");
+MPI_Finalize();
+return 0;
+
+}
+
+//Compilar MPI - mpicc mpi.c -o saida.out
+
+//Rodar MPI    - mpirun ./saida.out
