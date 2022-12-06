1abf23f1ad7e97e79f3df722b92c4f9cac2cd5cc
Alexandre Filho <45580663+AlexandreMFilho@users.noreply.github.com>
2022-12-06 19:48:28 -0300

    Update mpi.c

--- MPI/mpi.c
+++ MPI/mpi.c
@@ -3,20 +3,39 @@
 
 int main(int argc, char ** argv){
 
-int numtask, rank, rc;
+    int numtask, rank, rc, dest,source,tag=1,count;
+    char outmsg, inmsg;
+    MPI_Status stat;
 
-rc = MPI_Init(&argc,&argv);
-//Teste Segurança
-if(rc != MPI_SUCCESS){
-    printf("Error start MPI Program. Terminating..\n");
-    MPI_Abort(MPI_COMM_WORLD,rc);
-}
-MPI_Comm_size(MPI_COMM_WORLD,&numtask);
-MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-printf("Numero de task:%d Meu Ranking:%d\n",numtask,rank);
-    printf("Hello");
-MPI_Finalize();
-return 0;
+    rc = MPI_Init(&argc,&argv);
+    //Teste Segurança
+    if(rc != MPI_SUCCESS){
+        printf("Error start MPI Program. Terminating..\n");
+        MPI_Abort(MPI_COMM_WORLD,rc);
+    }
+    MPI_Comm_size(MPI_COMM_WORLD,&numtask);
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+    if(rank == 0){
+    dest = 1;
+    source = 1;
+    outmsg = 'x';
+    MPI_Send(&outmsg,1,MPI_CHAR,dest,tag,MPI_COMM_WORLD);
+    MPI_Recv(&inmsg,1,MPI_CHAR,source,tag,MPI_COMM_WORLD,&stat);
+    }
+    else{
+    dest =0;
+    source=0;
+    outmsg='y';
+    MPI_Recv(&inmsg,1,MPI_CHAR,source,tag,MPI_COMM_WORLD,&stat);
+    MPI_Send(&outmsg,1,MPI_CHAR,dest,tag,MPI_COMM_WORLD);
+    }
+
+    MPI_Get_count(&stat,MPI_CHAR,&count);
+    printf("Task:%d Recebeu%d char(s) da task %d with tag %d outmsg:%c inmsg:%c\n",rank,count,stat.MPI_SOURCE,stat.MPI_TAG,outmsg,inmsg);
+
+    MPI_Finalize();
+    return 0;
 
 }
 
@@ -25,3 +44,5 @@ return 0;
 //Rodar MPI    - mpirun ./saida.out
 
 //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
+
+//https://github.com/f2acode/jogo-da-vida
